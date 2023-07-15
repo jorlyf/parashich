9dcae318d06703970ef4bd53e55fe205e537343d
jorlyf <jorlyfchannel@gmail.com>
2023-07-15 21:37:25 +0400

    time converter inited.

---
+++ app/src/utils/time.ts
@@ -0,0 +1,11 @@
+export const convertTimeToUTC = (date: any) => {
+
+}
+
+export const convertTimeFromUTC = (date: any) => {
+
+}
+
+export const convertTimeFromTimestamp = (timestamp: number) => {
+  
+}
